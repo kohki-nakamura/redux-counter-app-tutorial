6f369aac05bf6ed3503f6a3543df94672a4c49e0
kohki-nakamura <42920380+kohki-nakamura@users.noreply.github.com>
2022-12-27 15:03:20 +0900

    redux練習

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
+import { increment, decrement, incrementByAmount } from './redux/counterSlice';
 
 function App() {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+  const [incrementAmount, setIncrementAmount] = useState("2");
+
   return (
-    <div className="App"></div>
+    <div className="App">
+      <h1>count: {count}</h1>
+      <input onChange={(e) => setIncrementAmount(e.target.value)} value={incrementAmount} />
+      <button onClick={() => dispatch(increment())}>＋</button>
+      <button onClick={() => dispatch(decrement())}>ー</button>
+      <button
+        onClick={() => dispatch(incrementByAmount(Number(incrementAmount)))}
+      >
+        追加
+      </button>
+    </div>
   );
 }
 
